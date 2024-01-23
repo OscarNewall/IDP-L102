03b05988c7fcd9356caccfdbe1269bac7779ac88
Oscar <on245@cam.ac.uk>
2024-01-23 16:55:03 +0000

    motor setup and setspeed function

---
+++ motor.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Adafruit_MotorShield.h>
+
+// To be added to main setup (initialising motors and checking they exist)
+
+Adafruit_MotorShield AFMSleft = Adafruit_MotorShield(/*I2C address leftmotor*/);
+Adafruit_MotorShield AFMSright = Adafruit_MotorShield(/*I2C address rightmotor*/);
+
+Adafruit_DCMotor* leftmotor = AFMSleft.getMotor(/*left motor port number*/);
+Adafruit_DCMotor* rightmotor = AFMSright.getMotor(/*right motor port number*/);
+
+Serial.begin(9600);
+if (!AFMSleft.begin()) {
+	Serial.println("Could not find left Motor Shield. Check wiring.");
+	while (1);
+}
+if (!AFMSright.begin()) {
+	Serial.println("Could not find right Motor Shield. Check wiring.");
+	while (1);
+}
+Serial.println("Both Motor Shields found.");
+
+
+// Function to set motor speeds
+// Use MOT_setspeeds(0,0) to stop
+
+void MOT_setspeeds(int leftspeed, int rightspeed) {
+    if (leftspeed < 0) {
+        leftmotor->run(BACKWARD);
+    }
+    else {
+        leftmotor->run(FORWARD)
+    }
+    if (rightspeed < 0) {
+        rightmotor->run(BACKWARD);
+    }
+    else {
+        rightmotor->run(FORWARD)
+    }
+	leftmotor->setSpeed(abs(leftspeed));
+	rightmotor->setSpeed(abs(rightspeed));
+}
