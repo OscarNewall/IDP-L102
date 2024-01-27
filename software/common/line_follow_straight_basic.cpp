43cb5b019bb72b4f6be7f3cf4ae86d75a52f8d0f
Bruno Iwanczyk <bi231@cam.ac.uk>
2024-01-27 22:37:43 +0000

    or and replaced with || &&

--- software/common/line_follow_straight_basic.cpp
+++ software/common/line_follow_straight_basic.cpp
@@ -2,19 +2,19 @@
 
 void straight_basic(int speed_high, int speed_low) {
     LS_data_t data = LS_read();
-    if (data.far_left == 1 or data.far_right == 1 or (data.left == 0 and data.right == 0))
+    if (data.far_left == 1 || data.far_right == 1 || (data.left == 0 && data.right == 0))
     {
         MOT_setspeeds(0, 0);
     }
-    else if (data.left == 1 and data.right == 1)
+    else if (data.left == 1 && data.right == 1)
     {
         MOT_setspeeds(speed_high, speed_high);
     }
-    else if (data.left == 1 and data.right == 0)
+    else if (data.left == 1 && data.right == 0)
     {
         MOT_setspeeds(speed_low, speed_high);
     }
-    else if (data.left == 0 and data.right == 1)
+    else if (data.left == 0 && data.right == 1)
     {
         MOT_setspeeds(speed_high, speed_low);
     }  
