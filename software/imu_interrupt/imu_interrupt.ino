a1f2d73893f78adba2f843fe2a82d7fd06cb3d3d
Bruno Iwanczyk <bi231@cam.ac.uk>
2024-01-29 13:29:23 +0000

    update

---
+++ software/imu_interrupt/imu_interrupt.ino
@@ -0,0 +1,77 @@
+#include <Adafruit_LSM6DS3.h>
+
+#define data_points 100 // how many datapoints to record in each array
+#define repeats 20 // how many arrays to send
+
+#define LSM_CS 30
+#define LSM_SCK 34
+#define LSM_MISO 33
+#define LSM_MOSI 32
+#define LSM_INT 31
+
+Adafruit_LSM6DS3 imu;
+float data[data_points];
+int index_datapoint = 0;
+int index_array = 0;
+
+void setup() {
+    Serial.begin(9600);
+    if(!imu.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+        Serial.println("Failed to find IMU");
+    }
+    imu.configIntOutputs(true, false);
+    imu.configInt1(false, true, false, false, false);
+    attachInterrupt(digitalPinToInterrupt(LSM_INT), callback_save_measurement, CHANGE);
+    /**
+    LSM6DS_RATE_12_5_HZ,
+    LSM6DS_RATE_26_HZ,
+    LSM6DS_RATE_52_HZ,
+    LSM6DS_RATE_104_HZ,
+    LSM6DS_RATE_208_HZ,
+    LSM6DS_RATE_416_HZ,
+    LSM6DS_RATE_833_HZ,
+    LSM6DS_RATE_1_66K_HZ,
+    LSM6DS_RATE_3_33K_HZ,
+    LSM6DS_RATE_6_66K_HZ,
+    */
+    imu.setAccelDataRate(LSM6DS_RATE_104_HZ);
+    imu.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+    /**
+    LSM6DS_ACCEL_RANGE_2_G,
+    LSM6DS_ACCEL_RANGE_16_G,
+    LSM6DS_ACCEL_RANGE_4_G,
+    LSM6DS_ACCEL_RANGE_8_G
+    */
+    imu.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+    /**
+    LSM6DS_GYRO_RANGE_125_DPS = 0b0010,
+    LSM6DS_GYRO_RANGE_250_DPS = 0b0000,
+    LSM6DS_GYRO_RANGE_500_DPS = 0b0100,
+    LSM6DS_GYRO_RANGE_1000_DPS = 0b1000,
+    LSM6DS_GYRO_RANGE_2000_DPS = 0b1100,
+    */
+    imu.setGyroRange(LSM6DS_GYRO_RANGE_125_DPS);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    /*
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    imu.getEvent(&accel, &gyro, &temp);
+    Serial.println(0);
+    Serial.println(accel.acceleration.x);
+    */
+    Serial.println(index_datapoint);
+    Serial.println(data[index_datapoint - 1]);
+}
+
+void callback_save_measurement() {
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    imu.getEvent(&accel, &gyro, &temp);
+    data[index_datapoint] = accel.acceleration.x;
+    index_datapoint++;
+}
