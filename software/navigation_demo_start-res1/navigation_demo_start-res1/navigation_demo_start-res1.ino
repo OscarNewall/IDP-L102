b57159f9c57cd57f77e3576f7e514952843be09a
Oscar <on245@cam.ac.uk>
2024-01-31 10:28:19 +0000

    nav demo ino file for start to res 1

---
+++ software/navigation_demo_start-res1/navigation_demo_start-res1/navigation_demo_start-res1.ino
@@ -0,0 +1,49 @@
+#include "src/line_follow_straight_basic.h"
+#include "src/junction_id.h"
+#include "src/navigation.h"
+
+Ticker bluetimer(LED_flashblue, 500, 0, MILLIS);
+
+void setup() {
+    Serial.begin(9600);
+    LS_setup();
+    MOT_initialise();
+    bluetimer.start();
+    state = NAV_next();
+}
+
+void loop() {
+
+    bluetimer.update(); // Run the movement LED for #safety :)
+
+    if (state == NAV_LINE_FOLLOW) {
+        if (!straight_basic(255,127)) {
+            state = NAV_next();
+            JUNC_ENTER();
+      }
+    }
+    else if (state == NAV_JUNC_LEFT) {
+        if (!JUNC_turn_loop(bool is_left = true) { // True for left
+            state = NAV_next();
+            // Don't need to JUNC_ENTER here as next state always NAV_LINE_FOLLOW
+        }
+    }
+    else if (state == NAV_JUNC_RIGHT) {
+        if (!JUNC_turn_loop(bool is_left = false) { // False for right
+            state = NAV_next();
+            // Don't need to JUNC_ENTER here as next state always NAV_LINE_FOLLOW
+        }
+    }
+    else if (state == BLIND_FORWARDS) {
+        if (!blind_forwards()) {
+            state = NAV_next();
+            // Don't need to JUNC_ENTER here as next state always NAV_LINE_FOLLOW
+        }
+    }
+    else if (state == JUNC_PASS) {
+        if (!JUNC_pass_loop()) {
+            state = NAV_next();
+            // Don't need to JUNC_ENTER here as next state always NAV_LINE_FOLLOW
+        }
+    }
+}
