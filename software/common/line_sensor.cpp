be531cd655bbca0970f2e79f29601637bf2ebe3a
Jon Tindal <jon.tindal@outlook.com>
2024-02-01 15:17:39 +0000

    Only log line sensors if data changed

--- software/common/line_sensor.cpp
+++ software/common/line_sensor.cpp
@@ -2,11 +2,22 @@
 
 #include "utils.h"
 
+static LS_data_t prev_data;
+
 void LS_setup() {
     pinMode(left_line_pin, INPUT);
     pinMode(far_left_line_pin, INPUT);
     pinMode(right_line_pin, INPUT);
     pinMode(far_left_line_pin, INPUT);
+}
+
+static bool data_unchanged(LS_data_t new_data) {
+    return (
+        prev_data.far_left == new_data.far_left &&
+        prev_data.left == new_data.left &&
+        prev_data.right == new_data.right &&
+        prev_data.far_right == new_data.far_right
+    );
 }
 
 LS_data_t LS_read() {
@@ -17,7 +28,10 @@ LS_data_t LS_read() {
     data.right = digitalRead(right_line_pin);
     data.far_right = digitalRead(far_right_line_pin);
 
-    UTIL_log(LOG_DEBUG, "LS: %u %u %u %u\n", data.far_left, data.left, data.right, data.far_right);
+    if (!data_unchanged(data)) {
+        UTIL_log(LOG_DEBUG, "LS: %u %u %u %u\n", data.far_left, data.left, data.right, data.far_right);
+    }
 
+    prev_data = data;
     return data;
 }
