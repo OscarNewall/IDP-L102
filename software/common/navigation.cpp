cae02a71ca142bdcf5323a0840d273e232cfcb59
Jon Tindal <jon.tindal@outlook.com>
2024-02-02 11:58:52 +0000

    Sleep after going through all navigation states

--- software/common/navigation.cpp
+++ software/common/navigation.cpp
@@ -1,5 +1,7 @@
 #include "navigation.h"
 
+#include "utils.h"
+
 char *states[] = {
     "NAV_JUNC_LEFT",
     "NAV_JUNC_RIGHT",
@@ -27,5 +29,10 @@ static const NAV_turns_e turns_order[] = {
 static unsigned int nav_pos_index = 0;
 
 NAV_turns_e NAV_next() {
+    // Stay in SLEEP if reached end of turns_order
+    if (nav_pos_index >= ARRAY_SIZE(turns_order)) {
+        UTIL_log(LOG_WARNING, "Reached end of states - at index %u\n", nav_pos_index);
+        return NAV_SLEEP;
+    }
     return turns_order[nav_pos_index++];
 }
