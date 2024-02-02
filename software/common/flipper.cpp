b0be98a1a2ead865373b2767459a383f6a2548aa
Oscar <on245@cam.ac.uk>
2024-02-02 12:14:29 +0000

    flipper/servo code and basic test file

--- software/common/flipper.cpp
+++ software/common/flipper.cpp
@@ -1,11 +1,9 @@
 #include "flipper.h"
-#include "utils.h"
-#include <Servo.h>
 
 Servo myservo;
 int previous_servo_pos = 0;
 int servo_pos = 0;
-int servo_pin;// NEED TO INPUT SERVO PIN NUMBER HERE
+int servo_pin;// NEED TO INPUT SERVO PIN NUMBER HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 void SERV_initialise() {
 // Function to be added to ino setup loop
@@ -14,19 +12,20 @@ void SERV_initialise() {
 
 void SERV_turn_to_angle(int target_pos) {
 // Function to turn the servo from its last position to a new target position, whether that is forwards or backwards
+// Will block anything else from running while the servo is turning - don't think this is a problem atm
     if (previous_servo_pos == target_pos) {
         return;
     }
     else if (previous_servo_pos < target_pos) {
         for (servo_pos = previous_servo_pos; servo_pos <= target_pos; servo_pos += 1) {
             myservo.write(servo_pos);
-            // Need to add delay here
+            delay(15);
         }
     }
     else if (previous_servo_pos > target_pos) {
         for (servo_pos = previous_servo_pos; servo_pos >= target_pos; servo_pos -= 1) {
             myservo.write(servo_pos);
-            // Need to add delay here
+            delay(15);
         }
     }
     previous_servo_pos = target_pos;
--- software/common/flipper.h
+++ software/common/flipper.h
@@ -1,6 +1,9 @@
 #ifndef flipper_h
 #define flipper_h
 
+#include <Servo.h>
+
 void SERV_initialise();
+void SERV_turn_to_angle(int);
 
 #endif
