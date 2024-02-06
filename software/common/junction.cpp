e8c04ece2830b94a97f9a3ef41d9cde67f779af2
Jon Tindal <jon.tindal@outlook.com>
2024-02-06 11:13:26 +0000

    Change junction turn logic to include blind turn time Time not tested

--- software/common/junction.cpp
+++ software/common/junction.cpp
@@ -10,6 +10,7 @@
 #define FORWARD_TIME_MS 350
 #define SHORT_FORWARD_TIME_MS 250 // For turns where the junction has been reversed into, so shorter step forward needed
 #define TURN_SPEED 200
+#define PARTIAL_TURN_TIME_MS 300  // Time for a ~60 degree turn
 
 bool JUNC_pass_loop() {
     if (!UTIL_reached_timeout(FORWARD_TIME_MS)) {
@@ -22,24 +23,25 @@ bool JUNC_pass_loop() {
 }
 
 bool JUNC_turn_loop(bool is_left, bool short_forward_step) {
+    /*
+    Should drive forward blindly for forward_time,
+    then turn blindly for PARTIAL_TURN_TIME_MS,
+    then continue turning until only both middle line sensors are on
+    */
     LS_data_t ls_out = LS_read();
 
-    if (short_forward_step) {
-        if (!UTIL_reached_timeout(SHORT_FORWARD_TIME_MS)) {
-            MOT_setspeeds(FORWARD_SPEED, FORWARD_SPEED);
-            return true;
-        }
-    }
-    else {
-        if (!UTIL_reached_timeout(FORWARD_TIME_MS)) {
-            MOT_setspeeds(FORWARD_SPEED, FORWARD_SPEED);
-            return true;
-        }
+    const unsigned long forward_time = short_forward_step ? SHORT_FORWARD_TIME_MS : FORWARD_TIME_MS;
+
+    if (!UTIL_reached_timeout(forward_time)) {
+        MOT_setspeeds(FORWARD_SPEED, FORWARD_SPEED);
+        return true;
     }
 
-    if (!ls_out.far_left && ls_out.left && ls_out.right && !ls_out.far_right) {
-        MOT_setspeeds(0, 0);
-        return false;
+    if (UTIL_reached_timeout(forward_time + PARTIAL_TURN_TIME_MS)) {
+        if (!ls_out.far_left && ls_out.left && ls_out.right && !ls_out.far_right) {
+            MOT_setspeeds(0, 0);
+            return false;
+        }
     }
 
     if (is_left) {
