efb00bb8819bb1d0cd3d6c6ecae048ade83a76f6
Oscar <on245@cam.ac.uk>
2024-02-12 11:34:08 +0000

    commenting + cleaning

--- software/run_state_machine/run_state_machine.ino
+++ software/run_state_machine/run_state_machine.ino
@@ -15,9 +15,8 @@ void setup() {
     SERV_initialise();
     bluetimer.start();
 
-    int startbuttonPin = 3; // can change
+    int startbuttonPin = 3;
     pinMode(startbuttonPin, INPUT);
-    
     while (1){
         int val = digitalRead(startbuttonPin);
         if (val == HIGH){
@@ -27,17 +26,10 @@ void setup() {
     }
 
     STATE_setup();
-
 }
 
 void loop() {
 
     bluetimer.update(); // Run the movement LED for #safety :)
     STATE_loop(); // Enact the list of states
-
-    /*
-    Use below code for seeing line sensor output
-    LS_data_t data = LS_read();
-    UTIL_log("%u %u %u %u \n",data.far_left,data.left,data.right,data.far_right);
-    */
 }
